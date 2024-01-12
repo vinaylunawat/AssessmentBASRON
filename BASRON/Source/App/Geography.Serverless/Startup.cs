be1565f9dca07b897aa010ede0d0a551690c358f
Vinay Lunawat <Vinay.Lunawat@rsystems.com>
2024-01-12 16:42:34 +0530

    changes

--- BASRON/Source/App/Geography.Serverless/Startup.cs
+++ BASRON/Source/App/Geography.Serverless/Startup.cs
@@ -24,7 +24,7 @@ public class Startup
         services.ConfigureClientServices();
         services.ConfigureGraphQLServices();
         services.ConfigureDataProvider();
-        services.ConfigureAwsCongnitoSecurity();
+        //services.ConfigureAwsCongnitoSecurity();
     }
 
     // This method gets called by the runtime. Use this method to configure the HTTP request pipeline
@@ -50,7 +50,8 @@ public class Startup
         app.UseEndpoints(endpoints =>
         {
             endpoints.MapControllers();
-            endpoints.MapGraphQL<AppSchema>().RequireAuthorization();
+            //endpoints.MapGraphQL<AppSchema>().RequireAuthorization();
+            endpoints.MapGraphQL<AppSchema>();
             endpoints.MapGet("/", async context =>
             {
                 await context.Response.WriteAsync("Welcome to running ASP.NET Core on AWS Lambda");
