4435c26a26deae004fdeaee57ed5879abfa25ff5
Sunil Ghutukade <Sunil.Ghutukade@grassvalley.com>
2024-01-12 16:03:07 +0530

    main resolved error while update

--- BASRON/Source/App/Geography.Business/BTransaction/Types/BTransactionInputType.cs
+++ BASRON/Source/App/Geography.Business/BTransaction/Types/BTransactionInputType.cs
@@ -27,13 +27,16 @@ namespace BASRON.Business.BTransaction.Types
         public BTransactionUpdateInputType()
         {
             Name = "btransactionUpdateInput";
-            Field<IdGraphType>("Id");
             Field<NonNullGraphType<StringGraphType>>("customerId");
-            Field<NonNullGraphType<DecimalGraphType>>("amount");
-            Field<NonNullGraphType<DateTimeGraphType>>("trasctionDate");
-            Field<NonNullGraphType<StringGraphType>>("trasctionType");
+            Field<NonNullGraphType<DateTimeGraphType>>("transactionDate");
             Field<NonNullGraphType<IdGraphType>>("referenceNumber");
-            Field<NonNullGraphType<StringGraphType>>("trasctionStatus");
+            Field<NonNullGraphType<DecimalGraphType>>("amount");
+            Field<NonNullGraphType<StringGraphType>>("transactionType");
+            Field<NonNullGraphType<StringGraphType>>("customerName");
+            Field<NonNullGraphType<StringGraphType>>("productType");
+            Field<NonNullGraphType<StringGraphType>>("currency");
+            Field<NonNullGraphType<DateTimeGraphType>>("createdDate");
+            Field<NonNullGraphType<DateTimeGraphType>>("updatedDate");
             Field<NonNullGraphType<BooleanGraphType>>("isActive");
         }
     }
